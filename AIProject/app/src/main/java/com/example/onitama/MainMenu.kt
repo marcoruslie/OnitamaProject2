37ed7bf4ea258513d3afe071a63e062854f86b07
andrewloekito123 <82990119+andrewloekito123@users.noreply.github.com>
2022-12-05 00:01:27 +0700

    Loading Screen

--- AIProject/app/src/main/java/com/example/onitama/MainMenu.kt
+++ AIProject/app/src/main/java/com/example/onitama/MainMenu.kt
@@ -3,6 +3,7 @@ package com.example.onitama
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.widget.Button
 
 class MainMenu : AppCompatActivity() {
@@ -10,6 +11,8 @@ class MainMenu : AppCompatActivity() {
     lateinit var btEasy : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main_menu)
         btVeryEasy = findViewById(R.id.btVeryEasy)
         btEasy = findViewById(R.id.btEasy)
